4ae422bdf8c6a5bcec418515f95139e25d7f0575
wanony <65881470+wanony@users.noreply.github.com>
2023-10-24 16:06:11 +0800

    update header link, add .gitignore

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -19,7 +19,7 @@
             </a>
         {/each}
     </div>
-    <a href="#" target="_blank" class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950">
+    <a href="mailto:whereweat2018@gmail.com" target="_blank" class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950">
         <div class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"></div>
         <h4 class="relative z-9">Get in touch</h4>
     </a>
